a17b8374acd207bd0be71a7f67a548da58b531fc
bennyty <bennytytyty@gmail.com>
2014-05-29 11:21:38 -0400

    First time startup complete

--- SpeedCoach/src/com/lehmannsystems/speedcoach/MainActivity.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/MainActivity.java
@@ -3,8 +3,10 @@ package com.lehmannsystems.speedcoach;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,28 +15,6 @@ import android.view.ViewGroup;
 
 public class MainActivity extends ActionBarActivity {
 
-	public void setPersonType (View v) {
-		SharedPreferences settings = getSharedPreferences("userPref", MODE_PRIVATE);
-		SharedPreferences.Editor prefEditor = settings.edit();
-		prefEditor.putBoolean("setup", true);
-		switch (v.getId()) {
-			case (R.id.SelectCoach):
-				prefEditor.putString("type", "Coach");
-				prefEditor.commit();
-				Intent intent = new Intent(this, CoachActivity.class);
-				startActivity(intent);
-			break;
-			case (R.id.SelectViewer):
-				prefEditor.putString("type", "Regatta Viewer");
-				prefEditor.commit();
-			break;
-			case (R.id.SelectCoxswain):
-				prefEditor.putString("type", "Coxswain");
-				prefEditor.commit();
-			break;
-		}
-		
-	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -44,8 +24,47 @@ public class MainActivity extends ActionBarActivity {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
+		
+		Intent intent;
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		//SharedPreferences.Editor prefEditor = prefs.edit();
+		//prefEditor.putBoolean("setup", Boolean.FALSE);
+		//prefEditor.commit();
+		
+	    boolean previouslyStarted = prefs.getBoolean("setup", Boolean.FALSE);
+	    Log.v(getLocalClassName(), String.valueOf(previouslyStarted));
+	    if(!previouslyStarted){
+		    intent = new Intent(this, WhoAreYouActivity.class);
+		    startActivity(intent);
+		    Log.d(getLocalClassName(), "This has Not been previously started");
+	    } else {
+	    	Log.d(getLocalClassName(), "This has been previously started");
+	    	switch (prefs.getInt("type", 0)) {
+		    	//0 - Coach
+				//1 - Cox
+				//2 - Viewer
+	    		case (0):
+	    			intent = new Intent(this, CoachActivity.class);
+					startActivity(intent);
+	    		break;
+	    		case (1):
+	    			intent = new Intent(this, CoxActivity.class);
+					startActivity(intent);
+				break;
+	    		case (2):
+	    			intent = new Intent(this, RegattaViewFullscreenActivity.class);
+	    			startActivity(intent);
+	    		break;
+	    		
+	    	}
+	    }
 	}
-
+	/*
+	@Override
+	protected void onResume() {
+		   
+	}
+	*/
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
