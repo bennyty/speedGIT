44256465afd6b9b7f302f5b471cbe4a51c7db5b8
scc-child <scc.child@gmail.com>
2014-05-29 09:42:58 -0400

    Fixed XML errors

--- SpeedCoach/src/com/lehmannsystems/speedcoach/CoachActivity.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/CoachActivity.java
@@ -3,6 +3,8 @@ package com.lehmannsystems.speedcoach;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -13,6 +15,11 @@ import android.os.Build;
 
 public class CoachActivity extends ActionBarActivity {
 
+	public void act (View v) {
+		//if (v.getId())
+	}
+		
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
