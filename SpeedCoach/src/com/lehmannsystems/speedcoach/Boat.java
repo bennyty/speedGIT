4aca37bfcbee74a0997433aab24de0bc7d72f41c
bennyty <bennytytyty@gmail.com>
2014-05-30 13:07:01 -0400

    JSON fixed and some warnings cleared

--- SpeedCoach/src/com/lehmannsystems/speedcoach/Boat.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/Boat.java
@@ -6,10 +6,7 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
-
-import android.widget.Toast;
 
 public class Boat {
 	String name;
--- SpeedCoach/src/com/lehmannsystems/speedcoach/MainActivity.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
