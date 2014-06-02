7b3a0c4b2d5447ed10c5326890e766a6cd348110
bennyty <bennytytyty@gmail.com>
2014-06-02 15:13:37 -0400

    Redone Cox location logic, no more NPEs

--- SpeedCoach/src/com/lehmannsystems/speedcoach/CoxActivity.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/CoxActivity.java
@@ -1,27 +1,21 @@
 package com.lehmannsystems.speedcoach;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.Fragment;
 import android.content.Context;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-public class CoxActivity extends ActionBarActivity implements LocationListener{
-	private TextView latituteField;
-	private TextView longitudeField;
-	private LocationManager locationManager;
-	private String provider;
-
+public class CoxActivity extends ActionBarActivity {
+	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,62 +23,31 @@ public class CoxActivity extends ActionBarActivity implements LocationListener{
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 
-		latituteField = (TextView) findViewById(R.id.tvLat);
-		longitudeField = (TextView) findViewById(R.id.tvLong);
+		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				location.getLatitude();
+				Toast.makeText(context, "Current speed:" + location.getSpeed(), Toast.LENGTH_SHORT).show();
+			}
 
-		// Get the location manager
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		// Define the criteria how to select the locatioin provider -> use default
-		//Criteria criteria = new Criteria();
-		provider = LocationManager.GPS_PROVIDER;;
-		Location location = locationManager.getLastKnownLocation(provider);
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
 
-		// Initialize the location fields
-		if (location != null) {
-			System.out.println("Provider " + provider + " has been selected.");
-			onLocationChanged(location);
-		} else {
-			//latituteField.setText("Location not available");
-			//longitudeField.setText("Location not available");
-		}
-	}
-	@Override
-	protected void onResume() {
-		super.onResume();
-		locationManager.requestLocationUpdates(provider, 400, 1, this);
-	}
+			public void onProviderEnabled(String provider) {
+			}
 
-	/* Remove the locationlistener updates when Activity is paused */
-	@Override
-	protected void onPause() {
-		super.onPause();
-		locationManager.removeUpdates(this);
-	}
-
-	public void onLocationChanged(Location location) {
-		int lat = (int) (location.getLatitude());
-		int lng = (int) (location.getLongitude());
-		latituteField.setText(String.valueOf(lat));
-		longitudeField.setText(String.valueOf(lng));
-	}
-
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
+			public void onProviderDisabled(String provider) {
+			}
+		};
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000,
+				0, locationListener);
 
 	}
 
-	public void onProviderEnabled(String provider) {
-		Toast.makeText(this, "Enabled new provider " + provider,
-				Toast.LENGTH_SHORT).show();
-	}
-
-	public void onProviderDisabled(String provider) {
-		Toast.makeText(this, "Disabled provider " + provider,
-				Toast.LENGTH_SHORT).show();
-	}
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
