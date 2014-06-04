e54fb9a245d1be9a51c4a769e4993622391ffeaf
bennyty <bennytytyty@gmail.com>
2014-06-04 10:10:00 -0400

    Cox Functionality
    Stopping Threads onClose
    Updates Database (Probably)
    Prompts the User to enable GPS

--- SpeedCoach/src/com/lehmannsystems/speedcoach/CoxActivity.java
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/CoxActivity.java
@@ -1,6 +1,14 @@
 package com.lehmannsystems.speedcoach;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
@@ -12,6 +20,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class CoxActivity extends ActionBarActivity implements GPSInterface {
 	Context context;
@@ -33,20 +42,64 @@ public class CoxActivity extends ActionBarActivity implements GPSInterface {
 
 		if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
-			.add(R.id.container, new PlaceholderFragment()).commit();
+					.add(R.id.container, new PlaceholderFragment()).commit();
 		}
 
-		myBoat = new Boat("Entheos Tester", "Mike", 1);
-		locResults = new float[1];
 		oldLatitude = 200;
 		oldLongitude = 200;
+	}
+
+	protected void onStart() {
+		super.onStart();
+		myBoat = new Boat("Entheos Tester", "Mike", 1);
+		locResults = new float[1];
 
 		// Acquire a reference to the system Location Manager
-		LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, this);
+
+		if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			Toast.makeText(this, "GPS is Enabled in your device.",
+					Toast.LENGTH_SHORT).show();
+		} else {
+			showGPSDisabledAlertToUser();
+		}
 
 		updateThread.start();
 		guiThread.start();
+	}
+
+	private void showGPSDisabledAlertToUser() {
+		AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+		alertDialogBuilder
+				.setMessage(
+						"GPS is disabled in your device. Would you like to enable it?")
+				.setCancelable(false)
+				.setPositiveButton("Goto Settings Page To Enable GPS",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								Intent callGPSSettingIntent = new Intent(
+										android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+								startActivity(callGPSSettingIntent);
+							}
+						});
+		alertDialogBuilder.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = alertDialogBuilder.create();
+		alert.show();
+	}
+
+	protected void onStop() {
+		super.onStop();
+
+		updateThread.interrupt();
+		guiThread.interrupt();
 	}
 
 	private void reset(Location loc) {
@@ -59,7 +112,7 @@ public class CoxActivity extends ActionBarActivity implements GPSInterface {
 	Thread updateThread = new Thread() {
 		public void run() {
 			while (!isInterrupted()) {
-				if (loc!=null) {
+				if (loc != null) {
 					if (oldLatitude == 200 || oldLongitude == 200) {
 						oldLatitude = loc.getLatitude();
 						oldLongitude = loc.getLongitude();
@@ -67,13 +120,30 @@ public class CoxActivity extends ActionBarActivity implements GPSInterface {
 					} else {
 						newLatitude = loc.getLatitude();
 						newLongitude = loc.getLongitude();
-						Location.distanceBetween(oldLatitude, oldLongitude, newLatitude, newLongitude, locResults);
+						Location.distanceBetween(oldLatitude, oldLongitude,
+								newLatitude, newLongitude, locResults);
 						myBoat.setMeters((int) locResults[0]);
 						oldLatitude = newLatitude;
 						oldLongitude = newLongitude;
 					}
 
 					myBoat.setSplitSeconds(500 / loc.getSpeed());
+
+					try {
+						URL db = new URL(
+								"http://www.getgreenrain.com/RowSplit/storeSplit.php?"
+										+ "cid=" + myBoat.getCox()
+										+ "&tid=" + myBoat.getTeamId()
+										+ "&spm=" + myBoat.getRate()
+										+ "&mps=" + myBoat.getRawSplit()
+										+ "&mt=" + myBoat.getMeters());
+						URLConnection dbURLConnection = db.openConnection();
+						dbURLConnection.connect();
+					} catch (MalformedURLException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
 				}
 			}
 		}
@@ -87,11 +157,14 @@ public class CoxActivity extends ActionBarActivity implements GPSInterface {
 						TextView display = (TextView) findViewById(R.id.tvMeters);
 						display.setText(myBoat.getMeters() + " m");
 						display = (TextView) findViewById(R.id.tvSplit);
-						display.setText(myBoat.formatSplit(myBoat.getRawSplit()) + "");
+						display.setText(myBoat.formatSplit(myBoat.getRawSplit())
+								+ "");
 						display = (TextView) findViewById(R.id.tvTime);
-						display.setText(myBoat.formatSplit(myBoat.getRawTime()) + "");
+						display.setText(myBoat.formatSplit(myBoat.getRawTime())
+								+ "");
 						display = (TextView) findViewById(R.id.tvAvgSplit);
-						display.setText(myBoat.formatSplit(myBoat.getRawAvgSplit()) + " ");
+						display.setText(myBoat.formatSplit(myBoat
+								.getRawAvgSplit()) + " ");
 						display = (TextView) findViewById(R.id.tvRate);
 						display.setText(myBoat.getRate() + "spm");
 					}
@@ -100,32 +173,26 @@ public class CoxActivity extends ActionBarActivity implements GPSInterface {
 		}
 	};
 
-	public void onLocationChanged(Location loc)
-	{
-		if (loc != null)
-		{
+	public void onLocationChanged(Location loc) {
+		if (loc != null) {
 			this.loc = loc;
 		}
 	}
 
-	public void onProviderDisabled(String provider)
-	{
+	public void onProviderDisabled(String provider) {
 		// TODO: do something one day?
 	}
 
-	public void onProviderEnabled(String provider)
-	{
+	public void onProviderEnabled(String provider) {
 		// TODO: do something one day?
 	}
 
-	public void onStatusChanged(String provider, int status, Bundle extras)
-	{
+	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO: do something one day?
 
 	}
 
-	public void onGpsStatusChanged(int event)
-	{
+	public void onGpsStatusChanged(int event) {
 		// TODO: do something one day?
 	}
 
