950041c246bdd644a94c190905bbaba2911b7182
scc-child <scc.child@gmail.com>
2014-06-09 14:52:58 -0400

    pls

---
+++ SpeedCoach/src/com/lehmannsystems/speedcoach/CoachLoginActivity.java
@@ -0,0 +1,321 @@
+package com.lehmannsystems.speedcoach;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Activity which displays a login screen to the user, offering registration as
+ * well.
+ */
+public class CoachLoginActivity extends Activity {
+
+	/**
+	 * The default email to populate the email field with.
+	 */
+	public static final String EXTRA_EMAIL = "I DONT KNOW WTF THIS IS";
+
+	/**
+	 * Keep track of the login task to ensure we can cancel it if requested.
+	 */
+	private UserLoginTask mAuthTask = null;
+
+	// Values for email and password at the time of the login attempt.
+	private String mName;
+	private String mTeam;
+
+	// UI references.
+	private EditText mNameView;
+	private AutoCompleteTextView mTeamView;
+	private View mLoginFormView;
+	private View mLoginStatusView;
+	private TextView mLoginStatusMessageView;
+
+	List<String> teamNames;
+	HashMap<String, Integer> teamNamesWithId;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		setContentView(R.layout.activity_coach_login);
+
+		// Set up the login form.
+		mName = getIntent().getStringExtra(EXTRA_EMAIL);
+		mNameView = (EditText) findViewById(R.id.etCoxName);
+		mNameView.setText(mName);
+
+		mTeamView = (AutoCompleteTextView) findViewById(R.id.actvCoxTeam);
+		mTeamView
+				.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+					@Override
+					public boolean onEditorAction(TextView textView, int id,
+							KeyEvent keyEvent) {
+						if (id == R.id.login || id == EditorInfo.IME_NULL) {
+							attemptLogin();
+							return true;
+						}
+						return false;
+					}
+				});
+
+		mLoginFormView = findViewById(R.id.login_form);
+		mLoginStatusView = findViewById(R.id.login_status);
+		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
+
+		findViewById(R.id.sign_in_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+
+		teamNames = new ArrayList<String>();
+		teamNamesWithId = new HashMap<String, Integer>();
+
+		new getTeamNames().execute((Void) null);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		getMenuInflater().inflate(R.menu.coach_login, menu);
+		return true;
+	}
+
+	/**
+	 * Attempts to sign in or register the account specified by the login form.
+	 * If there are form errors (invalid email, missing fields, etc.), the
+	 * errors are presented and no actual login attempt is made.
+	 */
+	public void attemptLogin() {
+		if (mAuthTask != null) {
+			return;
+		}
+
+		// Reset errors.
+		mNameView.setError(null);
+		mTeamView.setError(null);
+
+		// Store values at the time of the login attempt.
+		mName = mNameView.getText().toString();
+		mTeam = mTeamView.getText().toString();
+
+		boolean cancel = false;
+		View focusView = null;
+
+		// Check for a valid password.
+		if (TextUtils.isEmpty(mTeam)) {
+			mTeamView.setError(getString(R.string.error_field_required));
+			focusView = mTeamView;
+			cancel = true;
+		} /*
+		 * else if (mPassword.length() < 4) {
+		 * mPasswordView.setError(getString(R.string.error_invalid_password));
+		 * focusView = mPasswordView; cancel = true; }
+		 */
+
+		// Check for a valid email address.
+		if (TextUtils.isEmpty(mName)) {
+			mNameView.setError(getString(R.string.error_field_required));
+			focusView = mNameView;
+			cancel = true;
+		} /*
+		 * else if (!mEmail.contains("@")) {
+		 * mEmailView.setError(getString(R.string.error_invalid_email));
+		 * focusView = mEmailView; cancel = true; }
+		 */
+
+		if (cancel) {
+			// There was an error; don't attempt login and focus the first
+			// form field with an error.
+			focusView.requestFocus();
+		} else {
+			// Show a progress spinner, and kick off a background task to
+			// perform the user login attempt.
+			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			showProgress(true);
+			mAuthTask = new UserLoginTask();
+			mAuthTask.execute((Void) null);
+		}
+	}
+
+	/**
+	 * Shows the progress UI and hides the login form.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+	private void showProgress(final boolean show) {
+		// On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+		// for very easy animations. If available, use these APIs to fade-in
+		// the progress spinner.
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+			int shortAnimTime = getResources().getInteger(
+					android.R.integer.config_shortAnimTime);
+
+			mLoginStatusView.setVisibility(View.VISIBLE);
+			mLoginStatusView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 1 : 0)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginStatusView.setVisibility(show ? View.VISIBLE
+									: View.GONE);
+						}
+					});
+
+			mLoginFormView.setVisibility(View.VISIBLE);
+			mLoginFormView.animate().setDuration(shortAnimTime)
+					.alpha(show ? 0 : 1)
+					.setListener(new AnimatorListenerAdapter() {
+						@Override
+						public void onAnimationEnd(Animator animation) {
+							mLoginFormView.setVisibility(show ? View.GONE
+									: View.VISIBLE);
+						}
+					});
+		} else {
+			// The ViewPropertyAnimator APIs are not available, so simply show
+			// and hide the relevant UI components.
+			mLoginStatusView.setVisibility(show ? View.VISIBLE : View.GONE);
+			mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
+		}
+	}
+
+	private void add() {
+		// TODO Auto-generated method stub
+		ArrayAdapter<String> adp = new ArrayAdapter<String>(getBaseContext(),
+				android.R.layout.simple_dropdown_item_1line, teamNames);
+		adp.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+		mTeamView.setThreshold(1);
+		mTeamView.setAdapter(adp);
+	}
+
+	public class getTeamNames extends AsyncTask<Void, Void, Boolean> {
+		protected ProgressDialog pDialog;
+
+		@Override
+		protected void onPreExecute() {
+			pDialog = new ProgressDialog(CoachLoginActivity.this);
+			pDialog.setMessage("Please wait..");
+			pDialog.setIndeterminate(true);
+			pDialog.setCancelable(false);
+			pDialog.show();
+		};
+
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			try {
+				URL db = new URL(
+						"http://getgreenrain.com/RowSplit/getTeamList.php");
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						db.openStream()));
+				String inputLine = in.readLine();
+				JSONArray json = new JSONArray(inputLine); // .substring(1,
+				// inputLine.length()-1)
+				in.close();
+				for (int i = 0; i < json.length(); i++) {
+					JSONObject jo = json.getJSONObject(i);
+					String n = jo.getString("name");
+					String joId = jo.getString("id");
+
+					teamNamesWithId.put(n, Integer.valueOf(joId));
+					teamNames.add(n);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				return false;
+			}
+
+			return true;
+
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean ss) {
+			// TODO Auto-generated method stub
+			add();
+			pDialog.dismiss();
+		}
+	}
+
+	public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+		@Override
+		protected Boolean doInBackground(Void... params) {
+			String cTeamId = null;
+			for (String key : teamNamesWithId.keySet()) {
+				if (mTeam.equals(key)) {
+					cTeamId = teamNamesWithId.get(key).toString();
+				}
+			}
+
+			URL db;
+			BufferedReader in;
+			try {
+				// Network access
+				db = new URL(
+						"http://getgreenrain.com/RowSplit/insertCoxin.php?tid="
+								+ cTeamId + "&name=" + mName);
+				in = new BufferedReader(new InputStreamReader(db.openStream()));
+				in.readLine();
+				in.close();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				return false;
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				return false;
+			}
+
+			return true;
+		}
+
+		@Override
+		protected void onPostExecute(final Boolean success) {
+			mAuthTask = null;
+			showProgress(false);
+			if (success) {
+				Intent intent = new Intent(CoachLoginActivity.this,
+						CoachActivity.class);
+				startActivity(intent);
+				finish();
+			} else {
+				mTeamView.setError(getString(R.string.dummy_content));
+				mTeamView.requestFocus();
+			}
+		}
+
+		@Override
+		protected void onCancelled() {
+			mAuthTask = null;
+			showProgress(false);
+		}
+	}
+}
